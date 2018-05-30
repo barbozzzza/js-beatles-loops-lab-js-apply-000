616965a74f8c33da1a9b92ed0bf09403829e8d16
jose barboza <jrbarboza08@gmail.com>
2018-05-30 03:57:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function theBeatlesPlay() {
+  
+  var instruments = [
+    
+    'guitar',
+    'bass',
+    'drums',
+    'rhythm guitar',
+    ];
+    
+    
+  var MusiciansArray = [
+    
+    'Ringo Star',
+    'Paul Macarthny',
+    'Gorge Harrison',
+    'John Lennon',
+    
+    
+    
+    ];  
+  
+}
+  
+function JohnnLennonFacts(){
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+}
